2b5bf75276786909dbd12e8fc9a7d3b478b510c5
xritik <singhritik00901@gmail.com>
2025-03-23 17:55:00 +0530

    default user and pass added

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -5,13 +5,24 @@ const User = require('../models/user');
 router.post('/', async (req, res) => {
     const {name, password} = req.body;
 
+    const allUsers = await User.find();
     const user = await User.findOne({name, password});
 
-    if(user){
-        res.status(200).json({ message: 'Login Successful!!' });
-    } else{
-        res.status(401).json({ message: 'Invalid name or password!!' })
+    if (allUsers.length==0){
+        if(name=='user' && password=='password'){
+            res.status(200).json({ message: 'Login Successful!!' });
+        }else{
+            res.status(401).json({ message: 'Invalid name or password!!' });
+        }
+    }else{
+        if(user){
+            res.status(200).json({ message: 'Login Successful!!' });
+        } else{
+            res.status(401).json({ message: 'Invalid name or password!!' });
+        }
     }
+
+
 });
 
 module.exports = router;
