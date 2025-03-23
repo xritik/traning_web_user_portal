ac9fc178fb35d37fcefcb4fe8a68acc924e630ee
xritik <singhritik00901@gmail.com>
2025-03-23 16:15:23 +0530

    cors changed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const PORT = 5000;
 require('./db');
 
 const corsOptions = {
-    origin: 'http://localhost:3000',  // Allow only requests from this origin
+    origin: 'http://portal.vikasweb.xyz:3000',  // Allow only requests from this origin
+    // origin: 'http://localhost:3000',  // Allow only requests from this origin
     credentials: true,                // Allow credentials (cookies) to be sent
 };
 app.use(cors(corsOptions));
