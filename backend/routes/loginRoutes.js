141956cec1da56737b064989666570524a0c5043
xritik <singhritik00901@gmail.com>
2025-03-30 09:08:49 +0530

    changes into loginRoutes

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -9,7 +9,7 @@ router.post('/', async (req, res) => {
     const user = await User.findOne({name, password});
 
     if (allUsers.length==0){
-        if(name=='user' && password=='password'){
+        if(name=='user' && password=='pass'){
             res.status(200).json({ message: 'Login Successful!!' });
         }else{
             res.status(401).json({ message: 'Invalid name or password!!' });
